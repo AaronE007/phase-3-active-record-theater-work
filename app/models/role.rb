feb48a6ac5d9e3d04f958a03313cb899dfe45b09
Aaron Elias <aaronelias007@gmail.com>
2022-07-05 19:16:51 -0400

    Checking Solution

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,12 +1,13 @@
 class Role < ActiveRecord::Base
-  has_many : auditions
+  has_many :auditions
 
   def auditions
     self.audtions
   end 
 
   def actors
-    self.actor
+    self.auditions.map{ | each_audition| each_audition.actor }
+    binding.pry
   end 
 
 
